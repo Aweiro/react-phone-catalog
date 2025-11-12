85b6857327fb425cc897363723cff6a7ca9a13f5
Aweiro <arsenskott@gmail.com>
2025-11-12 16:27:06 +0200

    clone phone catalog shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './modules/shared/layout/Header';
+// eslint-disable-next-line max-len
+import { HeaderProvider } from './modules/shared/layout/Header/context/HeaderContext';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './modules/shared/layout/Footer';
+import { useConditionalScroll } from './hooks/useConditionalScroll';
+import { SaveProductsProvider } from './context/SaveProductsContext';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  useConditionalScroll();
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <SaveProductsProvider>
+      <Provider store={store}>
+        <div className="App">
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+
+          <main className="App-content">
+            {/* <ProductsProvider>
+            </ProductsProvider> */}
+            <Outlet />
+          </main>
+
+          <Footer />
+        </div>
+      </Provider>
+    </SaveProductsProvider>
   );
 };
